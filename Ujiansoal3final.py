7f5da0cf18d50f97cf23fbb4bfe6cfed2deed066
Satrio Dirgantoro <riotonks@gmail.com>
2020-09-23 21:36:49 +0700

    Update Ujiansoal3final.py

--- Ujiansoal3final.py
+++ Ujiansoal3final.py
@@ -15,18 +15,16 @@ print(urai('Purwadhika'))
 # PPyPytPythPythoPython
 # PPuPurPurwPurwaPurwadPurwadhPurwadhiPurwadhikPurwadhika
 
-def rajut(teks):                     # buat nama function dan parameter
-    baru = []                        # buat variable list kosong
-    for i in range(len(teks)-1):     # loop i sepanjang karakter teks -1
-        if teks[i] not in baru:      # jika teks[i] tidak ada di variable list baru
-            baru.append(teks[i])     # maka teks[i] di append k baru (hasil baru akan unique dan kurang 1 huruf paling belakang)
-    baru ="".join(baru) + teks[-1]   # setelah loop selesai baru di join dan ditambah slicing teks -1 (teks[-1] adalah huruf yg kurang)
-    return baru
 
+def rajut(teks):                 # Buat nama function dan parameter
+    new = teks.split(teks[0])    # Split teks dengan separator teks[0] dan store ke variable new (teks[0] karena setiap kata yg diulang dimulai dari itu)
+    new = teks[0]+new[-1]        # Lalu new sudah menjadi list dan kita overwrite nilainya dengan teks[0] ditambah cont-
+    return(new)                  # ditambah new[-1] (elemen hasil split yang paling belakang 'urwadhika')
 
 print(rajut('CCoCodCode'))
 print(rajut('PPyPytPythPythoPython'))
 print(rajut('PPuPurPurwPurwaPurwadPurwadhPurwadhiPurwadhikPurwadhika'))
+
 
 # Output:
 # Code
